a4cf772df9d89590da9bf50971b3dba63b1cb3ab
Jack Faller <jack.t.faller@gmail.com>
2026-07-18 21:35:21 +0100

    Moves scripts to root

---
+++ flash.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -ex
+cd "$(dirname "$(realpath "$0")")"
+KEYBOARD="$1"
+KEYMAP="$2"
+case "$3" in
+    right | "")
+        SIDE=right
+        FLIPPED=_flipped
+        ./make_flipped.sh "$KEYBOARD" "$KEYMAP"
+        ;;
+    left)
+        SIDE=left
+        FLIPPED=""
+        ;;
+esac
+qmk flash -j 4 -kb "$KEYBOARD" -km "$KEYMAP$FLIPPED" --bootloader "dfu-util-split-$SIDE"
--- make_flipped.sh
+++ make_flipped.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 set -ev
-FLIPPER=$(dirname $(realpath "$0"))/json_flipper
-LOCATION=$(realpath "$1")
-KEYMAP=$(basename "$LOCATION")
-cd $(dirname "$LOCATION")
+cd "$(dirname "$(realpath "$0")")"
+KEYBOARD="$1"
+KEYMAP="$2"
+LOCATION="./keyboards/$KEYBOARD/keymaps/$KEYMAP"
 # You can plug the USB into either half if you flash the right half with a map
 # that has all the keys mirrored along the vertical axis.
 rm -rf "$LOCATION"_flipped
 cp -r "$LOCATION" "$LOCATION"_flipped
 rm "$LOCATION"_flipped/keymap.json
-"$FLIPPER" "$LOCATION"/keymap.json > "$LOCATION"_flipped/keymap.json
+./json_flipper "$LOCATION"/keymap.json > "$LOCATION"_flipped/keymap.json
